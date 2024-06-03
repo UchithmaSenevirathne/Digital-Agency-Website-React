19431fb0fddcf6f7211467b7161b1e9e071dfccf
UchithmaSenevirathne <uchithma@gmail.com>
2024-06-03 21:25:58 +0530

    create side menu section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
     <div className='overflow-x-hidden'>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,91 +1,89 @@
-import React, {useState} from 'react'
-import Logo from '../../assets/website/Logo.svg';
-import DarkMode from './DarkMode';
+import React, { useState } from "react";
 import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
+import ResponsiveMenu from "./ResponsiveMenu";
+import Logo from "../../assets/website/Vector.svg";
+import DarkMode from "./DarkMode";
 
-const MenuLinks = [
-    {
-        id: 1,
-        name: "About",
-        link: "/#about",
-      },
-      {
-        id: 2,
-        name: "Services",
-        link: "/#services",
-      },
-      {
-        id: 3,
-        name: "Projects",
-        link: "/#projects",
-      },
-]
-
+export const MenuLinks = [
+  {
+    id: 1,
+    name: "About",
+    link: "/#about",
+  },
+  {
+    id: 2,
+    name: "Services",
+    link: "/#services",
+  },
+  {
+    id: 3,
+    name: "Projects",
+    link: "/#projects",
+  },
+];
 const Navbar = () => {
-    
-    const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
-      };
-      
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
-    <>
-        <nav className='duration-300 bg-white dark:bg-black dark:text-white'>
-            <div className="container py-3 md:py-2">
-                <div className="flex items-center justify-between">
-                    {/* logo section */}
-                    <div>
-                        <a href="#" className='flex items-center gap-3'>
-                            <img src={Logo} alt="logo" className='w-5'/>
-                            <span className='text-2xl font-semibold sm:text-3xl'>
-                                Digital Agency
-                            </span>
-                        </a>
-                    </div>
-                    {/*desktop navlinks section */}
-                    <div className='hidden md:block'>
-                        <ul className='flex items-center gap-8'>
-                            {
-                            MenuLinks.map(({id, name, link}) => {
-                                    return(
-                                    <li key={id} className='py-4 cursor-pointer'>
-                                        <a href="#"
-                                         className='py-2 text-lg font-medium transition-all duration-300 hover:text-primary hover:border-b-2 hover:border-primary'>
-                                            {name}
-                                        </a>
-                                    </li>
-                                    );
-                                })
-                            }
-                            
-                            <button className='primary-btn'>Get In Touch</button>
-                            <DarkMode/>
-                        </ul>
-                    </div>
-                    {/*mobile view*/}
-                    <div className='flex items-center gap-4 md:hidden'>
-                        <DarkMode/> 
-                        {showMenu ? (
-                            <HiMenuAlt1
-                                onClick={toggleMenu}
-                                className="transition-all cursor-pointer "
-                                size={30}
-                            />
-                        ) : (
-                        <HiMenuAlt3
-                            onClick={toggleMenu}
-                            className="transition-all cursor-pointer"
-                            size={30}
-                        />
-                        )}
-                        
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </>
-  )
-}
+    <div
+      className="relative z-10 w-full duration-300 dark:bg-black dark:text-white "
+    >
+      <div className="container py-3 md:py-2">
+        <div className="flex items-center justify-between">
+          {/* Logo section */}
+          <a
+            target="_blank"
+            href="https://www.youtube.com/channel/UC1H-a1MKEFXRiFlGNLcy7gQ?sub_confirmation=1"
+            className="flex items-center gap-3"
+          >
+            <img src={Logo} alt="" className="w-5" />
+            <span className="text-2xl font-semibold sm:text-3xl">
+              Digital agency
+            </span>
+          </a>
+          {/* Desktop view Navigation */}
+          <nav className="hidden md:block">
+            <ul className="flex items-center gap-8">
+              {MenuLinks.map(({ id, name, link }) => (
+                <li key={id} className="py-4">
+                  <a
+                    href={link}
+                    className="py-2 text-lg font-medium transition-colors duration-500  hover:text-primary hover:border-b-2 hover:border-primary"
+                  >
+                    {name}
+                  </a>
+                </li>
+              ))}
+              <button className="primary-btn">Get in Touch</button>
+              <DarkMode />
+            </ul>
+          </nav>
+          {/* Mobile view Drawer  */}
+          <div className="flex items-center gap-4 md:hidden ">
+            <DarkMode />
+            {/* Mobile Hamburger icon */}
+            {showMenu ? (
+              <HiMenuAlt1
+                onClick={toggleMenu}
+                className="transition-all cursor-pointer "
+                size={30}
+              />
+            ) : (
+              <HiMenuAlt3
+                onClick={toggleMenu}
+                className="transition-all cursor-pointer"
+                size={30}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+      <ResponsiveMenu showMenu={showMenu} />
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
