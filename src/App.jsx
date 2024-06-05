dcd9fd5013da5051771d522ba637e5a88e15736c
UchithmaSenevirathne <uchithma@gmail.com>
2024-06-05 19:18:01 +0530

    create testimonial section

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from "./components/Hero/Hero";
 import BrandLogo from './components/BrandLogo/BrandLogo';
 import Services from './components/Services/Services';
+import Testimonial from './components/Testimonial/Testimonial';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
   return (
-    <div className='overflow-x-hidden'>
+    <div className='overflow-x-hidden duration-300 bg-white dark:bg-black'>
       <Navbar/>
       <Hero/>
       <BrandLogo/>
       <Services/>
+      <Testimonial/>
     </div>
   )
 }
